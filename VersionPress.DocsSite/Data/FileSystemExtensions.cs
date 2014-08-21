e43c55e216b3ded65d4a30420ddff2338fdd51ce
Borek Bernard <borekb@gmail.com>
2014-08-21 13:59:51 +0200

    [WP-85] Navigation generated for docs content. Uses MvcSiteMap package and Jekyll-style front matter annotations (we'll probably switch to titles based on H1s soon)

--- VersionPress.DocsSite/Data/FileSystemExtensions.cs
+++ VersionPress.DocsSite/Data/FileSystemExtensions.cs
@@ -9,6 +9,9 @@ namespace VersionPress.DocsSite.Data
 {
     public static class FileSystemExtensions
     {
+
+        private static readonly Regex cleanNamePattern = new Regex(@"(?:\d{2}-)?([^\.]*)(?:\.md)?");
+
         /// <summary>
         /// Returns true if FileSystemInfo's name without the optional two-digit prefix
         /// and the `.md` extension matches the article name. For example, `02-getting-started.md`
@@ -17,11 +20,21 @@ namespace VersionPress.DocsSite.Data
         public static bool MatchesArticlePathPart(this FileSystemInfo fileSystemInfo, string articlePathPart)
         {
             var fileSystemEntryName = fileSystemInfo.Name;
-            var fileNamePattern = new Regex(@"(?:\d{2}-)?([^\.]*)(?:\.md)?");
-            var match = fileNamePattern.Match(fileSystemEntryName);
+            var match = cleanNamePattern.Match(fileSystemEntryName);
 
             return match.Success && match.Groups[1].Value == articlePathPart;
 
+        }
+
+        /// <summary>
+        /// Returns the file name as suitable for URL. For example, `02-getting-started.md`
+        /// becomes just `getting-started`.
+        /// </summary>
+        /// <param name="fileSystemInfo"></param>
+        /// <returns></returns>
+        public static string GetNameWithoutPrefix(this FileSystemInfo fileSystemInfo)
+        {
+            return cleanNamePattern.Match(fileSystemInfo.Name).Groups[1].Value;
         }
 
     }
---
+++ VersionPress.DocsSite/Data/MenuNodeProvider.cs
@@ -0,0 +1,55 @@
+﻿using MvcSiteMapProvider;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace VersionPress.DocsSite.Data
+{
+    public class MenuNodeProvider : DynamicNodeProviderBase 
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+
+            var baseDir = new DirectoryInfo(HostingEnvironment.MapPath("~/App_Data/content/en"));
+
+            var directories = baseDir.EnumerateDirectories().OrderBy(dir => dir.Name);
+            var keyPrefix = "content_";
+
+            foreach (var directory in directories)
+            {
+
+                var indexFile = new FileInfo(Path.Combine(directory.FullName, "_index.md"));
+                var frontMatter = indexFile.YamlHeader();
+
+                var dynamicNode = new DynamicNode();
+                dynamicNode.Title = (string)frontMatter["sectionTitle"];
+                dynamicNode.Url = "/en/" + directory.GetNameWithoutPrefix();
+                dynamicNode.Key = keyPrefix + directory.GetNameWithoutPrefix();
+                var parentKey = dynamicNode.Key;
+
+                yield return dynamicNode;
+
+                var files = directory.EnumerateFiles().Where(f => f.Name != "_index.md").OrderBy(f => f.Name);
+
+                foreach (var file in files)
+                {
+                    frontMatter = file.YamlHeader();
+
+                    dynamicNode = new DynamicNode();
+                    dynamicNode.Title = (string)frontMatter["title"];
+                    dynamicNode.Url = "/en/" + directory.GetNameWithoutPrefix() + "/" + file.GetNameWithoutPrefix();
+                    dynamicNode.ParentKey = parentKey;
+
+                    yield return dynamicNode;
+                }
+
+
+            }
+
+
+        }
+    }
+}
