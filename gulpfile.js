474f1e269566dcdde8b775269a0d056e0beff336
Borek Bernard <borekb@gmail.com>
2014-12-10 14:20:19 +0100

    CSS files are now auto-prefixed using autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 /// <binding AfterBuild='less' />
 var gulp = require('gulp');
 var less = require('gulp-less-sourcemap');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer-core');
+var sourcemaps = require('gulp-sourcemaps');
 
 var docsSite = './VersionPress.DocsSite';
 
-gulp.task('less', function () {
-    gulp.src(docsSite + '/css/**.less')
-      .pipe(less())
-      .pipe(gulp.dest(docsSite + '/css'));
+gulp.task('less', ['less-only', 'autoprefix']);
+
+gulp.task('less-only', function () {
+    return gulp.src(docsSite + '/css/**.less')
+        .pipe(less())
+        .pipe(gulp.dest(docsSite + '/css'));
+});
+
+gulp.task('autoprefix', ['less-only'], function () {
+    return gulp.src(docsSite + '/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer({ browsers: ['last 2 version'] })]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(docsSite + '/css'));
 });
