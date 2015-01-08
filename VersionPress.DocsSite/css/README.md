d5ad916a9ccfe4e24e950287116b640019b3c08c
Borek Bernard <borekb@gmail.com>
2015-01-08 15:51:12 +0100

    [WP-241] Docs site now uses the same look & feel and stylesheet structure as the main site (incl. gulpfile, VS post-build events etc.)

---
+++ VersionPress.DocsSite/css/README.md
@@ -0,0 +1,1 @@
+See [wiki](http://wiki.agilio.cz/versionpress:website#stylopisy-less-css) on how to structure stylesheets
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,77 @@
-/// <vs AfterBuild='less' />
 var gulp = require('gulp');
-var less = require('gulp-less-sourcemap');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer-core');
+var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
+var changed = require('gulp-changed');
+var autoprefixer = require('less-plugin-autoprefix');
+var gulpif = require('gulp-if');
+var del = require('del');
+var runSequence = require('run-sequence');
+var gutil = require('gulp-util');
 
-var docsSite = './VersionPress.DocsSite';
 
-gulp.task('less', ['less-only', 'autoprefix']);
+var projectDir = "./VersionPress.DocsSite";
 
-gulp.task('less-only', function () {
-    return gulp.src(docsSite + '/css/**.less')
-        .pipe(less())
-        .pipe(gulp.dest(docsSite + '/css'));
+/**
+ * Default task reports usage only
+ */
+gulp.task('default', function (cb) {
+    gutil.log('');
+    gutil.log('Usage:');
+    gutil.log(' ' + gutil.colors.green('gulp less') + '             Quick compilation of changed files only');
+    gutil.log(' ' + gutil.colors.green('gulp less-production') + '  Full compilation incl. autoprefixing etc.');
 });
 
-gulp.task('autoprefix', ['less-only'], function () {
-    return gulp.src(docsSite + '/css/*.css')
+
+// These vars can be set in 'prepare-production-less-build' to influence LESS build
+var lessPlugins = [];
+var compileOnlyChangedFiles = true;
+
+/**
+ * Basic LESS task. Runs less + sourcemaps only by default. See also 'less-production' to
+ * include autoprefixer etc.
+ */
+gulp.task('less', function () {
+
+    var destFolder = projectDir + '/css';
+
+    return gulp.src(projectDir + '/css/style-*.less')
+        .pipe(gulpif(compileOnlyChangedFiles, changed(destFolder, { extension: '.css' })))
         .pipe(sourcemaps.init())
-        .pipe(postcss([autoprefixer({ browsers: ['last 2 version'] })]))
+        .pipe(less({
+            plugins: lessPlugins,
+            relativeUrls: true // so that e.g. a link to the bg-noise image works from the 'lib' directory
+        }))
+        .on('error', function (error) {
+            console.log(error);
+        })
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(docsSite + '/css'));
+        .pipe(gulp.dest(destFolder));
+
 });
+
+/**
+ * Compiles LESS for production. Is generally quite a bit slower as all stylesheet are recompiled
+ * and more work is done on them, e.g., vendor prefixes are added etc.
+ */
+gulp.task('less-production', function (cb) {
+    runSequence('prepare-production-less-build', 'clean-css', 'less', cb);
+});
+
+/**
+ * Sets variables that influence the 'less' task
+ */
+gulp.task('prepare-production-less-build', function () {
+    lessPlugins = [
+        new autoprefixer()
+    ];
+    compileOnlyChangedFiles = false;
+});
+
+/**
+ * Cleans *.css and *.css.map files
+ */
+gulp.task('clean-css', function (cb) {
+    del([projectDir + '/css/style-*.css', projectDir + '/css/style-*.map'], { force: true }, cb);
+});
+
+
