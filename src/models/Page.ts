2cdada1a9093f90a4030cbffbc4c8fc69f7b0b97
Ivan Doležal <ivan.dolezal@gmail.com>
2016-01-23 18:32:12 +0100

    #4 - change in Page domain object to correct side menu rendering, rendering of menu via mixin

--- src/models/Page.ts
+++ src/models/Page.ts
@@ -1,14 +1,20 @@
 /// <reference path="DocsArticle.ts" />
+/// <reference path="Route.ts" />
 import {BaseModel} from "./BaseModel";
+import {Route} from './Route';
 import {DocsArticle} from './DocsArticle';
+
 export class Page extends BaseModel{
 
     version: string;
     docsArticle: DocsArticle;
+    rootRoute: Route;
 
-    constructor(article: DocsArticle, version: string) {
+    constructor(article: DocsArticle, version: string, rootRoute:Route) {
         super();
         this.docsArticle = article;
         this.version = version;
+        this.rootRoute = rootRoute;
     }
+
 }
