81cc05fb500b4f4a17e5152b873da26267bae173
Ivan Doležal <ivan.dolezal@gmail.com>
2016-01-23 18:30:03 +0100

    update #7 - front matter parsing, app config parsing, config.yaml parsing for creating correct main navigation

--- src/services/ConfigServiceClass.ts
+++ src/services/ConfigServiceClass.ts
@@ -7,32 +7,59 @@ import readline = require("readline");
 
 export class ConfigServiceClass {
 
-     private static _instance:ConfigServiceClass = new ConfigServiceClass();
+    private static _instance:ConfigServiceClass = new ConfigServiceClass();
 
-        private _appConfig: any;
+    private _appConfig: any;
 
-        private _config_file_name = "config.yaml";
+    private _docsDir: string;
 
-        constructor() {
-            if (ConfigServiceClass._instance) {
-                throw new Error("Error: Instantiation failed: Use ConfigServicClass.getInstance() instead of new.");
+    private _config_file_name = "config.yaml";
+
+    constructor() {
+        if (ConfigServiceClass._instance) {
+            throw new Error("Error: Instantiation failed: Use ConfigServicClass.getInstance() instead of new.");
+        }
+        ConfigServiceClass._instance = this;
+        console.log("ConfigServiceClass initialized");
+        this._init();
+    }
+
+    public static getInstance():ConfigServiceClass {
+        return ConfigServiceClass._instance;
+    }
+
+    public static getDirConfig(file: string){
+        return YAML.load(file);
+    }
+
+    public static getFrontMatter(file: string) {
+        var data = fs.readFileSync(file, 'utf8');
+        var lines = data.split("\n");
+        var fMatter = "";
+        if(lines.length>0) {
+            if (lines[0]==='---') {
+                for(var i = 1, iMax = lines.length; i < iMax; i++){
+                    if(lines[i]==='---') {break;}
+                    fMatter+=lines[i];
+                }
+                return YAML.parse(fMatter);
+            } else {
+                return null;
             }
-            ConfigServiceClass._instance = this;
-            console.log("ConfigServiceClass initialized");
-            this._init();
         }
+    }
 
-        public static getInstance():ConfigServiceClass {
-            return ConfigServiceClass._instance;
-        }
-    
+    private _init():void {
+        this._docsDir =  process.env.DOCS_SOURCE_FOLDER || '.';
+        this._appConfig = YAML.load(path.resolve(this._docsDir,this._config_file_name));
+    }
 
-        private _init():void {
-            const docs_root_folder = process.env.DOCS_SOURCE_FOLDER || '.';
-            this._appConfig = YAML.load(path.resolve(docs_root_folder,this._config_file_name));
-        }
+    get docsDir(){
+        return this._docsDir;
+    }
 
-        get appConfig() {
-            return this._appConfig;
-        }
+
+    get appConfig() {
+        return this._appConfig;
+    }
 }
