08f2dfab7f7cf456a5e21addc00bddf9ac5337bc
Ivan Doležal <ivan.dolezal@gmail.com>
2016-01-23 18:33:06 +0100

    #3 - filling of docsArticle for page with correct markdown rendered content

--- src/controllers/example.ts
+++ src/controllers/example.ts
@@ -5,7 +5,11 @@
 import { Request, Response } from 'express';
 import {DocsArticle} from '../models/DocsArticle';
 import {Page} from '../models/Page';
+import {Route} from '../models/Route';
+import {Language} from '../models/Language';
 import {ConfigServiceClass} from '../services/ConfigServiceClass';
+import {RoutingServiceClass} from '../services/RoutingServiceClass';
+import {renderDocument} from '../services/RenderService';
 
 module Example {
 
@@ -15,14 +19,26 @@ module Example {
      * Return an empty 200 response
      */
     export function printMessage (req: Request, res: Response) {
+        var language: string = req.path.split("/")[1];
+        return _printMessage(req,res,language);
+    }
+
+    function _printMessage(req: Request, res: Response, language: string){
         var cfg : ConfigServiceClass = ConfigServiceClass.getInstance();
+        var rs : RoutingServiceClass = RoutingServiceClass.getInstance();
         let docsArticle = new DocsArticle();
-        docsArticle.title=req.params.message;
-        let page = new Page(docsArticle,cfg.config.displayVersion);
+        docsArticle.title=req.params.article;
+        docsArticle.language=Language[language];
+        var route:Route = rs.getRouteByUrl(req.path);
+        docsArticle.content=renderDocument(route.path);
+        docsArticle.title = route.title;
+        console.log(route);
+        let page = new Page(docsArticle,cfg.appConfig.displayVersion,rs.getRoutesForLanguage(language));
+        console.log(page);
+        //console.log(rs.getRouteByUrl(req.path))
         res.status(200).render('index', page);
 
     }
-
 }
 
 export = Example;
