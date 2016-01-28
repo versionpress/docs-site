734c1a086892ee8494d01af15ea04fd409895ee0
Ivan Doležal <ivan.dolezal@gmail.com>
2016-01-28 07:25:41 +0100

    #25 - adjusted RenderService so it is stripping front matter before passing it to marked for rendering output

--- src/services/RenderService.ts
+++ src/services/RenderService.ts
@@ -5,11 +5,31 @@ import fs = require("fs");
 module RenderService{
 
     export function renderDocument(file:string) {
-         var output;
+         var output:string;
          var data = fs.readFileSync(file, 'utf8');
-        output =  Marked(data.toString());
+        output =  Marked(removeFrontMatter(data).toString());
         //console.log(output);
         return output;
+    }
+
+    function removeFrontMatter(data:string) {
+        var lines = data.split("\n");
+        var lnLength  = lines.length;
+        if(lnLength>0) {
+            if (lines[0]==='---') {
+                lines.splice(0,1);
+                var i = 1;
+                while(i<lnLength) {
+                    if(lines[0]==='---') {
+                        lines.splice(0,1);
+                        break;
+                    }
+                    lines.splice(0,1);
+                    i++;
+                }
+            } 
+        }
+        return lines.join("\n");
     }
 
 }
