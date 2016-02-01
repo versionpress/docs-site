02f8fa70e76ec8f24409b521bc177a79cd95e529
Ivan Doležal <ivan.dolezal@gmail.com>
2016-02-01 09:01:46 +0100

    #29 - when accessing site root, user is redirected to first available localization of the docs-site.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -12,12 +12,17 @@ let router = Router();
 
 // Simple argument passing
 var service = RoutingServiceClass.getInstance();
+
 for(var language of service.languages) {
     router.get('/'+language+'/*', renderPage);
     router.get('/'+language, renderPage);
 }
 
 router.get('/sitemap.xml', renderSitemap);
+
+router.get('/', function(request, response) {
+    response.redirect(301,'/'+service.languages[0]);
+});
 
 
 
